67ce425d33dbf19f65e6d4c2d8c05be3ea479bf8
Sachia <szucspalmi@gmail.com>
2019-11-19 18:27:01 +0100

    Patrolling enemy
    Patrolling agent now patrols and detects Jeremy

---
+++ UnityGame/Assets/Scripts/GunControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunControl : MonoBehaviour
+{
+    CapsuleCollider collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("HIT" + other);
+    }
+}
