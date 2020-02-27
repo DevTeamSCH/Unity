1a86bf329ab566a9b5f7a3e06005341a8401a7d0
Bence Cs <bence.csokas@gmail.com>
2020-02-27 13:45:45 +0100

    Foundational Collider setup

--- UnityGame/Assets/Scripts/JeremyController.cs
+++ UnityGame/Assets/Scripts/JeremyController.cs
@@ -4,22 +4,31 @@ using UnityEngine;
 
 public class JeremyController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public bool isTouchingGround;
+    private bool _isTouchingGround;
     private float speed;
     private float walkSpeed = 0.05f;
     private float runSpeed = 0.1f;
     private float turnSpeed = 1.5f;
     private float jumpHeight=5f;
+    
     Rigidbody rbody;
     Animator animator;
     CapsuleCollider capsuleCollider;
+    
+    public bool IsTouchingGround{
+        get{ return _isTouchingGround; }
+        set{
+            _isTouchingGround = value;
+            animator.SetBool("isTouchingGround", value);
+        }
+    }
+    
+    // Start is called before the first frame update
     void Start()
     {
         rbody = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
         capsuleCollider = GetComponent<CapsuleCollider>();
-        isTouchingGround = true;
         animator.SetBool("isWalking", false);
         animator.SetBool("isRunning", false);
         animator.SetBool("isIdle", true);
@@ -32,13 +41,12 @@ public class JeremyController : MonoBehaviour
         var y = Input.GetAxis("Horizontal") * turnSpeed;
         transform.Translate(0, 0, z);
         transform.Rotate(0, y, 0);
-        if (Input.GetKey(KeyCode.Space) && isTouchingGround)
+        if (Input.GetKey(KeyCode.Space) && IsTouchingGround)
         {
             Debug.Log("Jumpy jump!");
             rbody.AddForce(transform.up*jumpHeight,ForceMode.Impulse);
-            isTouchingGround = false;
-            animator.SetBool("isTouchingGround", false);
-            Debug.Log(isTouchingGround);
+            IsTouchingGround = false;
+            Debug.Log(IsTouchingGround);
         }
         else if (Input.GetKey(KeyCode.LeftShift))
         {
@@ -90,11 +98,12 @@ public class JeremyController : MonoBehaviour
         }
 
     }
-    void OnCollisionEnter()
+    void OnCollisionEnter(Collision coll)
     {
         Debug.Log("Collision!");
-        isTouchingGround = true;
-        animator.SetBool("isTouchingGround",true);
+        if(coll.gameObject.CompareTag("Ground"))
+            IsTouchingGround = true;
+        // else: check for Booster, Enemy, etc.
     }
 
 }
