f9556d3fad3c56166ac5a05630cdee514d7df144
SachiaTheRaven <43549644+SachiaTheRaven@users.noreply.github.com>
2020-09-02 13:01:13 +0200

    Jump implemented

--- UnityGame/Assets/Scripts/Controllers/JeremyController.cs
+++ UnityGame/Assets/Scripts/Controllers/JeremyController.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class JeremyController : MonoBehaviour
 {
-    private bool _isTouchingGround;
-    private float speed=6;
+    private float speed = 6;
     float rotationSmoothing = 0.1f;
     float rotationSmoothVelocity;
     public Transform cam;
+
+    Vector3 velocity;
+    public float gravity = -9.81f;
+
+    public Transform groundIndicator;
+    public float groundDistance = 1.0f;
+    public LayerMask groundMask;
+    private bool isGrounded;
+
 
     //TODO change to state machine
     bool fps = true;
@@ -16,48 +24,38 @@ public class JeremyController : MonoBehaviour
     private float walkSpeed = 0.05f;
     private float runSpeed = 0.1f;
     private float turnSpeed = 1.5f;
-    private float jumpHeight=5f;
+    private float jumpHeight = 1f;
 
     public CharacterController characterController;
-    
-    Rigidbody rbody;
-    Animator animator;
-    CapsuleCollider capsuleCollider;
-    
-    public bool IsTouchingGround{
-        get{ return _isTouchingGround; }
-        set{
-            _isTouchingGround = value;
-            animator.SetBool("isTouchingGround", value);
-        }
-    }
-    
+
+    public Animator animator;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        rbody = GetComponent<Rigidbody>();
-        animator = GetComponent<Animator>();
-        capsuleCollider = GetComponent<CapsuleCollider>();
-       // animator.SetBool("isWalking", false);
-     //animator.SetBool("isRunning", false);
-        //animator.SetBool("isIdle", true);
+        animator.SetBool("isWalking", false);
+        animator.SetBool("isRunning", false);
+        animator.SetBool("isIdle", true);
     }
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundIndicator.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0) velocity.y = -2;
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
         var direction = new Vector3(horizontal, 0, vertical).normalized;
 
         Vector3 moveDirection;
 
-        if(direction.magnitude>=0.1)
+        if (direction.magnitude >= 0.1)
         {
 
             if (fps)
             {
-                moveDirection = transform.right*horizontal+transform.forward*vertical;
+                moveDirection = transform.right * horizontal + transform.forward * vertical;
             }
             else
             {
@@ -67,9 +65,11 @@ public class JeremyController : MonoBehaviour
                 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             }
             characterController.Move(moveDirection.normalized * speed * Time.deltaTime);
-
-
         }
+        if (Input.GetButtonDown("Jump") && isGrounded) velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+
         /*transform.Translate(0, 0, horizontal);
         transform.Rotate(0, vertical, 0);
         if (Input.GetKey(KeyCode.Space) && IsTouchingGround)
@@ -131,10 +131,10 @@ public class JeremyController : MonoBehaviour
     }
     void OnCollisionEnter(Collision coll)
     {
-       /* Debug.Log("Collision!");
-        if(coll.gameObject.CompareTag("Ground"))
-            IsTouchingGround = true;
-        // else: check for Booster, Enemy, etc.*/
+        /* Debug.Log("Collision!");
+         if(coll.gameObject.CompareTag("Ground"))
+             IsTouchingGround = true;
+         // else: check for Booster, Enemy, etc.*/
     }
 
 }
