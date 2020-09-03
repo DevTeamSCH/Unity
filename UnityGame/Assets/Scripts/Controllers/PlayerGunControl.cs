37e0874b7cb60613a1a86792c7153defac4aebb9
SachiaTheRaven <43549644+SachiaTheRaven@users.noreply.github.com>
2020-09-03 14:31:59 +0200

    Shooting for Jeremy
    Jeremy now can shoot bullets and kill the Skateboard Loli

---
+++ UnityGame/Assets/Scripts/Controllers/PlayerGunControl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGunControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int damage = 10;
+    public float range = 40f;
+
+    public Camera fpsCam;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+        
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            HealthController hc = hit.transform.GetComponent<HealthController>();
+            if (hc != null)
+            {
+                hc.TakeDamage(damage);
+            }
+        }
+
+    }
+}
